c2891a954a29862dfe09b795ffd8856c2ce62dc6
Renata Rodrigues <renata-rm@hotmail.com>
2019-08-14 23:48:42 -0300

    Inicializando o projeto Lista-compras

---
+++ lista-compras/src/components/App.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import {
+  BrowserRouter,
+  Switch,
+  Route
+} from 'react-router-dom';
+
+import './App.scss';
+import Listas from './lista/Listas';
+import Lista from './lista/Lista';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <BrowserRouter basename={process.env.PUBLIC_URL}>
+          <Switch>
+            <Route exact path = "/" component = {Listas}/>
+            <Route exact path = "/" component = {Lista}/>
+
+          </Switch>
+        </BrowserRouter>
+
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ lista-compras/src/components/menu/Menu.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Menu extends Component {
+    render() {
+        return (
+            <div className="menu">
+                <span>{this.props.titulo}</span>
+            </div>
+        );
+    }
+}
+
+export default Menu;
