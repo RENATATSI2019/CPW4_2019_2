c3006ff8c61c37bcdc3be6cd4f040f547973d76a
Renata Rodrigues <renata-rm@hotmail.com>
2019-08-28 23:36:17 -0300

    Inicializando API e Estilizando Lista

--- lista-compras/src/components/lista/Listas.js
+++ lista-compras/src/components/lista/Listas.js
@@ -1,49 +1,65 @@
 import React, { Component } from 'react';
 
 import Menu from '../menu/Menu';
-import logo from'../../img/logo.png';
+import logo from '../../img/logo.png';
 import ListaService from '../../services/ListaService';
+import './Lista.scss';
+import adicionar from '../../img/add.png';
 
-class Listas extends Component {
-    constructor(){
+export default class Listas extends Component {
 
+    constructor() {
+        /**
+         * Toda vez que criar um construtor
+         * em um componente React, lembre-se
+         * de invocar o construtor da classe
+         * mãe Component na primeira linha de
+         * código.
+         */
         super();
-        this.state={
-            listas:[]
+
+        this.state = {
+            listas: []
         }
-        this.service=new ListaService();
+
+        this.service = new ListaService();
     }
-    //metodo que monta o componente antes de carregar os dados
-    componentDidMount(){
-        const listas = 
-        this.service.recuperarListas();
-        this.setState({listas});
+
+    async componentDidMount() {
+        const listas =
+            await this.service.recuperarListas();
+        console.log(listas);
+        this.setState({ listas });
     }
 
     render() {
-        const listas = this.state.listas.map(lista =>(
-            <li key={lista._id}>{lista.nome}</li>
-        ));
-
+        const listas =
+            this.state.listas.map(lista => (
+                <div className ="item" key={lista._id}>{lista.nome}</div>
+            ));
 
         return (
             <div>
                 <Menu
                     logo={logo}
                     paginaAnterior="/"
-                    titulo="Listas de Compras" />
-                
+                    titulo="Lista de Compras" />
+
                 <div className="conteiner">
                     <div>
-                        <h2>Minhas Listas</h2>
-                        <ul>
+                        <h2>Minhas listas</h2>
+
+                        <div id="listagem">
                             {listas}
-                        </ul>
+                        </div>
+
+                        <div id="areaBotao">
+                            <img src={adicionar} alt="Nova Lista" id="botaoNovaLista"/>
+                        </div>
+
                     </div>
                 </div>
             </div>
         );
     }
 }
-
-export default Listas;
